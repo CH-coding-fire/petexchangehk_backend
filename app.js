4172bb6a92988b9f6deca0918d84e8d2f84bf127
Coding-noob-helpMePlz <67637342+Coding-noob-helpMePlz@users.noreply.github.com>
2022-09-13 15:08:39 +0800

    tweak port to have process.env.port

--- app.js
+++ app.js
@@ -101,9 +101,9 @@ const httpsServer = https.createServer(httpsOptions, app)
 // })
 
 const connect_mode = 'express_vanilla'
-const PORT = 443;
+const PORT1 = 8080;
 if (connect_mode == 'express_vanilla') {
-	app.listen(PORT, () => {
+	app.listen(process.env.PORT || PORT1, () => {
 		const today = new Date();
 		console.log(
 			`*********  Express: LISTENING TO PORT: ${PORT} at ${today.getHours()}:${today.getMinutes()}:${today.getSeconds()} *********`
