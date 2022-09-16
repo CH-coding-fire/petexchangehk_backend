7b3a41d1f9ae404b8479e5620619f5d0934da2b2
Coding-noob-helpMePlz <67637342+Coding-noob-helpMePlz@users.noreply.github.com>
2022-09-16 22:56:50 +0800

    tweaked CORS to include FRONTEND_URL

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ app.use(bodyParser.text());
 
 app.use(
 	cors({
-		origin: 'http://localhost:3000',
+		origin: process.env.FRONTEND_URL||'http://localhost:3000',
 		methods: 'GET, POST, PUT,DELETE',
 		credentials: true,
 	})
