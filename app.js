c2d2e252ca3d573a447987d226c3016004fd593a
Coding-noob-helpMePlz <67637342+Coding-noob-helpMePlz@users.noreply.github.com>
2022-09-22 17:35:51 +0800

    added cookie secure true

--- app.js
+++ app.js
@@ -75,11 +75,12 @@ const sessionConfig = {
 	secret: 'thisshouldbeabettersecret!',
 	resave: false,
 	saveUninitialized: true,
-	// cookie: {
-	// 	// httpOnly: true,
-	// 	expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
-	// 	maxAge: 1000 * 60 * 60 * 24 * 7,
-	// },
+	cookie: {
+		// httpOnly: true,
+		expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+		maxAge: 1000 * 60 * 60 * 24 * 7,
+		cookie:{secure:true}
+	},
 };
 app.use(bodyParser.json());
 
