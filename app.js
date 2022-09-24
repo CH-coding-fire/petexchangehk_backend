d576ac1a44325c05fc83039f43bd54a068b9a5d7
Coding-noob-helpMePlz <67637342+Coding-noob-helpMePlz@users.noreply.github.com>
2022-09-24 14:41:46 +0800

    .

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ var corsOptions = {
 
 app.use(
 	cors({
-		origin: [process.env.FRONTEND_URL, 'http://localhost:3000'],
+		origin: process.env.FRONTEND_URL,
 		methods: 'GET, POST, PUT,DELETE',
 		credentials: true,
 	})
