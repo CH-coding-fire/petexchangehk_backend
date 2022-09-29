2fa78654eeb69ce1331677bec11dc80ae51db223
Coding-noob-helpMePlz <67637342+Coding-noob-helpMePlz@users.noreply.github.com>
2022-09-29 22:47:04 +0800

    added sameSite

--- app.js
+++ app.js
@@ -84,7 +84,7 @@ const sessionConfig = {
 		expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
 		maxAge: 1000 * 60 * 60 * 24 * 7,
 		// domain: thisServerURL,
-		// sameSite: 'strict'
+		sameSite: 'none'
 		// secure:true
 	},
 };
