6dcbeb2d59a5cb548b7dadb77c01e9988efac8f7
Coding-noob-helpMePlz <67637342+Coding-noob-helpMePlz@users.noreply.github.com>
2022-10-01 23:03:55 +0800

    .

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ app.use(bodyParser.json());
 		origin: targetClientURL,
 		credentials: true,
 	})
-);
+
 
 // app.use(function(req, res, next) {
 //   res.header('Access-Control-Allow-Origin', targetClientURL);
